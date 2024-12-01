30c03d89444ddd4fa8de0add70eb3ab2e9aab35e
drdata1010 <159292707+drdata1010@users.noreply.github.com>
2024-12-01 23:30:30 +0530

    01/12/24

---
+++ src/main/java/Arrays/SmallSubSumMoreThanK.java
@@ -0,0 +1,28 @@
+package Arrays;
+
+public class SmallSubSumMoreThanK {
+    public static int findSmallLength(int[]arr, int k){
+        int smallestLength = Integer.MAX_VALUE;
+        int left = 0;
+        int currSum = 0;
+        int n = arr.length;
+
+        for(int right = 0; right < n; right++){
+            //add current to currSUm
+            currSum += arr[right];
+
+            while(currSum > k){
+                smallestLength = Math.min(smallestLength, right - left + 1);
+                currSum -= arr[left];
+                left++;
+            }
+        }
+        return smallestLength == Integer.MAX_VALUE ? -1 : smallestLength;
+    }
+    public static void main(String[] args) {
+        int arr[] = {1, 4, 45, 6, 0, 19};
+        int k = 51;
+        int smallSub = findSmallLength(arr, k);
+        System.out.println(smallSub);
+    }
+}
